9a20081c0bd0efbf3533633c88ac52dfdbf74693
dmitrykoryanov <dmitry.koryanov@yandex.ru>
2023-04-01 12:27:32 +0200

    Zweiter Commit

--- src/main/java/com/infy/apache/HelloServlet.java
+++ src/main/java/com/infy/apache/HelloServlet.java
@@ -10,8 +10,11 @@ import java.time.LocalDateTime;
 
 public class HelloServlet extends HttpServlet {
 
+    public final String SOME_STRING_CONSTANT = "LA-LA-LA!";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello Servlet!");
         resp.getWriter().println("<html><h1>" + LocalDateTime.now() + "</h1></html>");
         //super.doGet(req, resp);
     }
